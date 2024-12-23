4f6509a8495b7e9c3ee7e112504156539cb95a9d
Aleksandr <62847375+DANTECK-dev@users.noreply.github.com>
2024-12-23 23:37:13 +0800

    update deps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,19 @@ import { EmployeeListComponent } from "./employee-list/employee-list.component";
 import {HttpClientModule} from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    //BrowserModule, 
-    EmployeeListComponent, 
-    // TODO: `HttpClientModule` should not be imported into a component directly.
-    // Please refactor the code to add `provideHttpClient()` call to the provider list in the
-    // application bootstrap logic and remove the `HttpClientModule` import from this component.
-    HttpClientModule
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+        CommonModule,
+        //BrowserModule, 
+        EmployeeListComponent,
+        // TODO: `HttpClientModule` should not be imported into a component directly.
+        // Please refactor the code to add `provideHttpClient()` call to the provider list in the
+        // application bootstrap logic and remove the `HttpClientModule` import from this component.
+        HttpClientModule
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'EMS (Angular × Spring Boot)';
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { EmployeeService } from '../employee.service';
 
 @Component({
-  selector: 'app-employee-list',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+    selector: 'app-employee-list',
+    imports: [CommonModule],
+    templateUrl: './employee-list.component.html',
+    styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent implements OnInit{
   
